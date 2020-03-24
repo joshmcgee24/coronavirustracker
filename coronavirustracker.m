b1f95cbafcb8dcb90f00943918e8a8e3d8c5d71c
joshmcgee24 <62305603+joshmcgee24@users.noreply.github.com>
2020-03-24 11:21:59 -0400

    Update coronavirustracker.m

--- coronavirustracker.m
+++ coronavirustracker.m
@@ -1,5 +1,5 @@
 % Coronavirus Tracker - Country Tracker - Joshua McGee
-% Created to track the spread of Coronavirus (COVID-19) 
+% Created to track the spread of Coronavirus (COVID-19)
 % Data is stored online and is provided via JHU CSSE from various sources including:
 % "the World Health Organization (WHO), DXY.cn. Pneumonia. 2020, BNO News,
 % National Health Commission of the People? Republic of China (NHC),
@@ -12,8 +12,8 @@
 
 %important settings:
 country = "US"; %specify country to model
-dp = 3; %number of days to predict for
 prediction_enabled = 1; %set to 1 for logistic model curve, 0 to turn off
+world_enabled = 1; %set to 1 to enable world statistics, 0 to turn off
 
 %Obtaining and formating data - courtesy of Toshi Takeuchi - https://www.mathworks.com/matlabcentral/profile/authors/951521
 result=webread('https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv','options','table');
@@ -82,6 +82,17 @@ for i = 1:cols1(2)
     Countrytotalinfected(i) = table2array(countrytable(1,i));
     Countrytotaldead(i) = table2array(countrytable1(1,i));
 end
+infected = Countrytotalinfected;
+startidx = zeros(length(infected));
+for i = 1:length(infected)
+    if i == length(infected)
+        break
+    end
+    if infected(i+1) > 1.5*infected(i) && infected(i) > 10
+        startidx(i) = i;
+    end
+end
+startidx = find(startidx~=0, 1, 'first');
 warning('off','all')
 last_day = datetime(countrytable.Properties.VariableNames(end),'InputFormat','MM/dd/yy');
 first_day = datetime(2020,1,22);
@@ -95,123 +106,145 @@ if prediction_enabled == 1
     myfun = 'y~A/(1+C*exp(-B*x))';
     tbl = table(x,y);
     model = fitnlm(tbl,myfun,beta0)
-    newtime = [0:1:daytotal+dp];
-    newdatetime = first_day:last_day+days(dp);
+    K = table2array(model.Coefficients(1,1));
+    r = table2array(model.Coefficients(2,1));
+    A = table2array(model.Coefficients(3,1));
+    C0  = fix(K/(A + 1));
+    tpeak  = fix(log(A)/r);
+    dpeak  = days(tpeak) + first_day;
+    dend   = 2*days(tpeak) + first_day;
+    dCpeak = fix(r*K/4);
+    tau    = fix(4/r);
+    newtime = [0:1:daytotal+21];
+    newdatetime = first_day:last_day+days(21);
     fitinfected1 = feval(model,newtime);
-    projected = fitinfected1(1,[numel(newdatetime)-(dp-1)]:numel(newdatetime));
+    projected = fitinfected1(1,[numel(newdatetime)-(21-1)]:numel(newdatetime));
     projectedeaths = max(fitinfected1)*Countrydeathrate/100;
     plot(newdatetime,fitinfected1,'r','LineWidth',2)
     hold on
     plot(time,Countrytotalinfected,'b*','MarkerSize',7)
     hold on
-    titlestr = sprintf('Predicted Coronavirus Cases in %s on %s',country,datetime(last_day+days(dp),'Format','yyyy-MM-dd'));
-    title(titlestr)
-    set(gca,'FontSize',11,'Fontweight','Bold')
-    legend('Predicted Cases','Data from John Hopkins')
-    xlabel('Date')
-    ylabel('Projected - Confirmed Cases')
-    str1 = sprintf('Total Projected: %0.0f | Total Projected Dead: %0.0f | R^2 = %0.3f',round(max(fitinfected1)),abs(round(projectedeaths)),model.Rsquared.Adjusted);
+    title(sprintf('COVID-19 Epidemic Simulation for %s',country))
+    str1 = sprintf('Total Projected: %0.0f | Total Projected Dead: %0.0f | R^2 = %0.3f \n Date: %s | Toal Cases: %0.0f | Total Deaths: %0.0f',K,Countrydeathrate*K,model.Rsquared.Adjusted,datestr(time(end)),Countrytotalinfected(end),Countrytotaldead(end));
     T = text(min(get(gca,'xlim')), max(get(gca,'ylim')), str1);
     set(T, 'fontsize', 10, 'verticalalignment', 'top', 'horizontalalignment', 'left');
+    legend('Predicted Cases','Data from John Hopkins','location','southwest')
+    xlabel('Date')
+    ylabel('Projected - Confirmed Cases')
 else
     plot(time,Countrytotalinfected,'b*','MarkerSize',7)
     hold on
     titlestr = sprintf('Coronavirus Cases in %s',string(country));
     title(titlestr)
     set(gca,'FontSize',11,'Fontweight','Bold')
-    legend('Data from John Hopkins')
+    legend('Data from John Hopkins','location','southwest')
     xlabel('Date')
     ylabel('Confirmed Cases')
     str2 = sprintf('Total Cases: %0.0f | Total Dead: %0.0f',round(max(Countrytotalinfected)),round(max(Countrytotaldead)));
     T = text(min(get(gca,'xlim')), max(get(gca,'ylim')), str2);
     set(T, 'fontsize', 10, 'verticalalignment', 'top', 'horizontalalignment', 'left');
 end
-fprintf('--------- %s Data ----------------- \n',country)
 caseperday = diff(Countrytotalinfected)./diff(day(time));
 firstday = datetime(2020,3,10);
 timematrix = firstday:last_day;
 T = table(caseperday(1,48:numel(Countrytotalinfected)-1)',timematrix');
-T.Properties.VariableNames = {'New Country Cases','Date'};
+T.Properties.VariableNames = {(sprintf('New %s Cases',country)),sprintf('Date')};
 T
 if prediction_enabled == 1
-    T = table(round(projected)',newdatetime(1,[numel(newdatetime)-(dp-1)]:numel(newdatetime))');
-    T.Properties.VariableNames = {'Projected Country Cases','Projected Date'};
+    fprintf('--------- %s Data ----------------- \n',country)
+    fprintf(' date: %10s  day: %3d\n',datestr(time(end)),numel(time));
+    fprintf(' start date: %s \n',time(startidx));
+    fprintf(' number of cases: %d\n',max(Countrytotalinfected));
+    fprintf(' number of deaths: %d\n',max(Countrytotaldead));
+    fprintf(' estimated epidemic size (cases): %d\n',K);
+    fprintf(' estimated epidemic rate (1/day): %d\n',r);
+    fprintf(' estimated initial state (cases): %d\n',C0);
+    fprintf(' estimated initial doubling time (day): %3.1f\n',round(log(2)/r,1));
+    fprintf(' estimated duration of fast growth phase (day): %d\n',tau);
+    fprintf(' estimated peak date: %s  day: %d \n',dpeak,tpeak);
+    fprintf(' estimated peak rate (cases/day): %d\n',dCpeak);
+    fprintf(' estimated end of transition phase: %s  day: %d\n',dend,2*tpeak);
+    T = table(round(projected)',round(projected'.*(Countrydeathrate/100)),newdatetime(1,[numel(newdatetime)-(21-1)]:numel(newdatetime))');
+    T.Properties.VariableNames = {(sprintf('Projected %s Cases',country)),(sprintf('Projected %s Deaths',country)),sprintf('Projected Date')};
     T
 end
 fprintf('As of: %s : ----------------------------------\n',last_day)
 fprintf('Infected: %0.0f, Dead: %0.0f, Death Rate: %0.4f \n',max(Countrytotalinfected),max(Countrytotaldead),Countrydeathrate)
 
-%plotting comparison of all countries
-countries = groupsummary(times_conf_country,"Country/Region", "max");
-vars = countries.Properties.VariableNames;
-countries = removevars(countries,[{'GroupCount'},vars(contains(vars,"remove"))]);
-countries1 = countries(:,5:end);
-cols1 = size(countries1);
-Countrytotalinfected = zeros(cols1(1),cols1(2));
-countries2 = groupsummary(times_conf_country1,"Country/Region", "max");
-vars = countries2.Properties.VariableNames;
-countries2 = removevars(countries2,[{'GroupCount'},vars(contains(vars,"remove"))]);
-countries3 = countries2(:,5:end);
-cols2 = size(countries3);
-Countrytotaldead = zeros(cols2(1),cols2(2));
-for i = 1:cols1(1)
-for j = 1:cols1(2)
-    Countrytotalinfected(i,j) = table2array(countries1(i,j));
-    Countrytotaldead(i,j) = table2array(countries3(i,j));
-end
-end
-T = table(countries(:,1),max(Countrytotalinfected')');
-T.Properties.VariableNames = {'Country','Cases'};
-sortrows(T,[2],{'descend'})
-[temp,originalpos] = sort(max(Countrytotalinfected')', 'descend' );
-
-figure
-%plot five countries with most cases
-for i = 2:6
-    plot(time,Countrytotalinfected(originalpos(i),:),'LineWidth',4);
+if world_enabled == 1
+    %plotting comparison of all countries
+    countries = groupsummary(times_conf_country,"Country/Region", "max");
+    vars = countries.Properties.VariableNames;
+    countries = removevars(countries,[{'GroupCount'},vars(contains(vars,"remove"))]);
+    countries1 = countries(:,5:end);
+    cols1 = size(countries1);
+    Countrytotalinfected = zeros(cols1(1),cols1(2));
+    countries2 = groupsummary(times_conf_country1,"Country/Region", "max");
+    vars = countries2.Properties.VariableNames;
+    countries2 = removevars(countries2,[{'GroupCount'},vars(contains(vars,"remove"))]);
+    countries3 = countries2(:,5:end);
+    cols2 = size(countries3);
+    Countrytotaldead = zeros(cols2(1),cols2(2));
+    for i = 1:cols1(1)
+        for j = 1:cols1(2)
+            Countrytotalinfected(i,j) = table2array(countries1(i,j));
+            Countrytotaldead(i,j) = table2array(countries3(i,j));
+        end
+    end
+    T = table(countries(:,1),max(Countrytotalinfected')');
+    T.Properties.VariableNames = {'Country','Cases'};
+    sortrows(T,[2],{'descend'})
+    [temp,originalpos] = sort(max(Countrytotalinfected')', 'descend' );
+    
+    figure
+    %plot five countries with most cases
+    for i = 2:6
+        plot(time,Countrytotalinfected(originalpos(i),:),'LineWidth',4);
+        hold on
+        legendInfo{i-1} = sprintf('%s',countries{originalpos(i), 1});
+    end
+    t = floor(now);
+    d = datetime(t,'ConvertFrom','datenum');
+    xlim([datetime(2020,2,15) d])
+    title('5 Countries with Most COVID-19 Cases')
+    set(gca,'FontSize',9,'Fontweight','Bold')
+    legend(legendInfo,'location','northwest')
+    
+    infected = zeros(10,1);
+    for i = 1:10
+        infected(i) = Countrytotalinfected(originalpos(i),end);
+        legendInfo2{i} = sprintf('%s',countries{originalpos(i), 1});
+    end
+    figure
+    P = pie(infected,legendInfo2);
     hold on
-    legendInfo{i-1} = sprintf('%s',countries{originalpos(i), 1}); 
+    pText = findobj(P,'Type','text');
+    percentValues = infected./(sum(infected))*100;
+    percentValues = mat2cell(percentValues',1,10);
+    txt = legendInfo2;
+    str1 = sprintf('Proportion of Cases | Total World Cases: %0.0f | Total World Dead: %0.0f',sum(sum(Countrytotalinfected(:,end))),sum(sum(Countrytotaldead(:,end))));
+    title(str1)
+    hold off
+    
+    T1 = table(countries(:,1),max(Countrytotaldead')');
+    T1.Properties.VariableNames = {'Country','Dead'};
+    sortrows(T1,[2],{'descend'})
+    [temp1,originalpos1] = sort(max(Countrytotaldead')', 'descend' );
+    
+    figure
+    %plot five countries with most deaths
+    for i = 2:6
+        plot(time,Countrytotaldead(originalpos1(i-1),:),'LineWidth',4);
+        hold on
+        legendInfo1{i-1} = sprintf('%s',countries{originalpos1(i-1), 1});
+    end
+    xlim([datetime(2020,2,15) d])
+    title('5 Countries with Most COVID-19 Deaths')
+    set(gca,'FontSize',9,'Fontweight','Bold')
+    legend(legendInfo1,'location','northwest')
+    hold off
+    
+    fprintf('\n ---- World Statistics for: %s \n',last_day)
+    fprintf('Total Cases: %d | Total Deaths: %d \n',sum(sum(Countrytotalinfected(:,end))),sum(sum(Countrytotaldead(:,end))))
+else
 end
-t = floor(now);
-d = datetime(t,'ConvertFrom','datenum');
-xlim([datetime(2020,2,15) d])
-title('5 Countries with Most COVID-19 Cases')
-set(gca,'FontSize',9,'Fontweight','Bold')
-legend(legendInfo)
-
-infected = zeros(10,1);
-for i = 1:10
-    infected(i) = Countrytotalinfected(originalpos(i),end);
-    legendInfo2{i} = sprintf('%s',countries{originalpos(i), 1}); 
-end
-figure
-P = pie(infected,legendInfo2);
-hold on
-pText = findobj(P,'Type','text');
-percentValues = infected./(sum(infected))*100;
-percentValues = mat2cell(percentValues',1,10);
-txt = legendInfo2;
-str1 = sprintf('Proportion of Cases | Total World Cases: %0.0f | Total World Dead: %0.0f',sum(sum(Countrytotalinfected(:,end))),sum(sum(Countrytotaldead(:,end))));
-title(str1)
-hold off
-
-T1 = table(countries(:,1),max(Countrytotaldead')');
-T1.Properties.VariableNames = {'Country','Dead'};
-sortrows(T1,[2],{'descend'})
-[temp1,originalpos1] = sort(max(Countrytotaldead')', 'descend' );
-
-figure
-%plot five countries with most deaths
-for i = 2:6
-    plot(time,Countrytotaldead(originalpos1(i-1),:),'LineWidth',4);
-    hold on
-    legendInfo1{i-1} = sprintf('%s',countries{originalpos1(i-1), 1}); 
-end
-xlim([datetime(2020,2,15) d])
-title('5 Countries with Most COVID-19 Deaths')
-set(gca,'FontSize',9,'Fontweight','Bold')
-legend(legendInfo1)
-hold off
-
-fprintf('\n ---- World Statistics for: %s \n',last_day)
-fprintf('Total Cases: %d | Total Deaths: %d \n',sum(sum(Countrytotalinfected(:,end))),sum(sum(Countrytotaldead(:,end))))
